50367ed4af4074c5efb643bf0c3d40255b4b92cd
stxxcsxnny <sk2993130@gmail.com>
2025-05-19 15:09:16 +0530

    connected to backenddd

--- app/main.py
+++ app/main.py
@@ -16,6 +16,8 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
+    allow_origins=["https://rsextractor.netlify.app"]
+
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
